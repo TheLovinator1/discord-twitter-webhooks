41528b1b6b0164f2863fada349e49137909c46fc
Joakim Hellsén <tlovinator@gmail.com>
2018-07-07 11:59:46 +0200

    Initial commit

---
+++ config.py
@@ -0,0 +1,21 @@
+# Webhook url
+# Right click a channel -> Edit channel -> Webhooks
+# Name will automatically be changed to the Twitter username
+url = "WEBHOOK_URL"
+
+# Go to http://apps.twitter.com and create an app.
+# The consumer key and secret will be generated for you after
+consumer_key = "CONSUMER_KEY"
+consumer_secret = "CONSUMER_SECRET"
+
+# After the step above, you will be redirected to your app's page.
+# Create an access token under the the "Your access token" section
+access_token = "ACCESS_TOKEN"
+access_token_secret = "ACCESS_TOKEN_SECRET"
+
+# Users to follow
+# A list of users to follow
+# Example: users_to_follow = ["719324795167309827", "1114707756", "36803580", "439480409"]
+# Needs to be the Twitter ID and not the username
+# https://tweeterid.com/
+users_to_follow = ["TWITTER_ID"]
