ef59efd33d4bfa0c28af4ba4f72c793195ebcff3
Joakim Hellsén <tlovinator@gmail.com>
2021-12-28 23:01:08 +0100

    Use twitter:image instead of og:image

--- discord_twitter_webhooks/get_meta_image.py
+++ discord_twitter_webhooks/get_meta_image.py
@@ -24,16 +24,17 @@ def get_meta_image(url: str) -> str:
         soup = BeautifulSoup(response.content, "html.parser")
 
         # TODO: Which one should be used if both are availabe?
+        # <meta property="og:image" content="">
+        og_image = soup.find_all("meta", attrs={"property": "og:image"})
+        logger.debug(f"og_image: {og_image}")
+        if og_image:
+            image_url = og_image[0].get("content")
+
         # <meta name="twitter:image" content="">
         twitter_image = soup.find_all("meta", attrs={"name": "twitter:image"})
         logger.debug(f"twitter_image: {twitter_image}")
         if twitter_image:
             image_url = twitter_image[0].get("content")
-
-        # <meta property="og:image" content="">
-        og_image = soup.find_all("meta", attrs={"property": "og:image"})
-        if og_image:
-            image_url = og_image[0].get("content")
 
     except Exception as exception:  # pylint: disable=broad-except
         logger.error(f"Error getting image url: {exception}")
