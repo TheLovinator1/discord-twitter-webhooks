64e98ee291a1acd488808eb2d286c6bcb42e9b9f
Joakim Hellsén <tlovinator@gmail.com>
2023-04-23 19:39:40 +0200

    Add pre-commit

---
+++ tests/test_settings.py
@@ -0,0 +1,22 @@
+from typing import TYPE_CHECKING
+
+from reader import Reader
+
+from discord_twitter_webhooks.settings import get_data_location, get_reader
+
+if TYPE_CHECKING:
+    from pathlib import Path
+
+
+def test_get_data_location() -> None:
+    """Test get_data_location."""
+    data_location: Path = get_data_location()
+    assert data_location.exists()
+    assert data_location.is_dir()
+
+
+def test_get_reader() -> None:
+    """Test get_reader."""
+    reader: Reader | None = get_reader()
+    assert reader is not None
+    assert isinstance(reader, Reader)
